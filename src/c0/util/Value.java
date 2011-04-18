f5ce681f5ee4bf2680001ef5e132a31afcc5160e
kimuraya <wired_rar@hotmail.com>
2011-04-18 23:27:31 +0900

    細かい修正を実施

--- src/c0/util/Value.java
+++ src/c0/util/Value.java
@@ -3,7 +3,7 @@ package c0.util;
 //値を管理するクラス
 public class Value {
 	private DataType dataType; //変数のデータ型
-	private int i; //整数
+	private int integer; //整数
 	private int[] array; //整数の配列
 	private String stringLiteral; //文字列定数
 }
