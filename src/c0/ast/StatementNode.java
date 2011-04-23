4c0dfd6f80d8da65a30e04e77993854e5b3659aa
kimuraya <jqf8jaz5xoi>
2011-04-23 21:09:12 +0900

    parser.jjなどを修正

--- src/c0/ast/StatementNode.java
+++ src/c0/ast/StatementNode.java
@@ -1,5 +1,17 @@
 package c0.ast;
 
 abstract public class StatementNode extends Node {
+	
+	protected Location location;
+	
+	public StatementNode(Location loc) {
+		super();
+		this.location = loc;
+	}
+
+	public Location location() {
+		return location;
+	}
+	
 	abstract public void dump(); //構文木を表示する
 }
