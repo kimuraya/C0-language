106fcb7d5f653131d0d0c7d263c7a9b09b177c32
kimuraya <wired_rar@hotmail.com>
2011-05-24 03:28:54 +0900

    Paser.jjに追記。主に変数宣言の箇所を修正。配列宣言子の箇所は削除する方向で検討。その為、言語仕様も一部修正される可能性がある。クラスDeclareVariableNodeに新しいメンバ変数を追加。

--- src/c0/ast/DeclareVariableNode.java
+++ src/c0/ast/DeclareVariableNode.java
@@ -1,20 +1,26 @@
 package c0.ast;
 
+import c0.util.DataType;
+
 //変数宣言
 //データ型 単純宣言子 ['=' 式] ';'
 //|データ型 配列宣言子 ';'
 public class DeclareVariableNode extends StatementNode {
 	
-	IdentifierNode identifier;
-	ExpressionNode expression;
+	private DataType dataType; //Nodeにするかどうか検討する
+	private IdentifierNode identifier;
+	private ExpressionNode expression; //初期化式。識別子の種類が配列だった場合、要素数を表す	
 	
-	public DeclareVariableNode(Location loc, IdentifierNode identifier,
+	//データ型 単純宣言子 ['=' 式] ';'
+	//|データ型 配列宣言子 ';'
+	public DeclareVariableNode(Location loc, DataType dataType, IdentifierNode identifier,
 			ExpressionNode expression) {
 		super(loc);
+		this.dataType = dataType;
 		this.identifier = identifier;
-		this.expression = expression;
-	}
-
+		this.expression = expression;		
+	}	
+	
 	@Override
 	public void dump() {
 		// TODO 自動生成されたメソッド・スタブ
