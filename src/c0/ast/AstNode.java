43dca9e4e34d0e3755ef2afa4823d66358052283
kimuraya <wired_rar@hotmail.com>
2011-06-06 00:38:49 +0900

    言語仕様を修正。外部宣言を削除。翻訳単位を修正。Parser.jjに追記

--- src/c0/ast/AstNode.java
+++ src/c0/ast/AstNode.java
@@ -6,13 +6,13 @@ import c0.util.NodeType;
 //抽象構文木のルート
 //グローバル変数の確保とmain関数の呼び出しを実行する
 public class AstNode extends Node {
-	private List<IdentifierNode> globalVariables; //グローバル変数のリスト
-	private IdentifierNode mainFunction; //main関数の参照
+	private List<DeclareVariableNode> globalVariables; //グローバル変数のリスト
+	private List<IdentifierNode> functions; //関数のリスト
 	
-	public AstNode(List<IdentifierNode> globalVariables,
-			IdentifierNode mainFunction) {
+	public AstNode(List<DeclareVariableNode> globalVariables,
+			List<IdentifierNode> functions) {
 		super();
 		this.globalVariables = globalVariables;
-		this.mainFunction = mainFunction;
+		this.functions = functions;
 	}	
 }
