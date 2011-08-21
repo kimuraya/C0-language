4d289e99be91fbef9feb7413215ef1b995044619
kimuraya <ra@localhost.localdomain>
2011-08-21 19:53:37 +0900

    クラスInterpreterをC0Interpreterに変更

---
+++ src/c0/interpreter/C0Interpreter.java
@@ -0,0 +1,63 @@
+package c0.interpreter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import c0.ast.AstNode;
+import c0.parser.C0Language;
+import c0.parser.ParseException;
+import c0.util.Identifier;
+
+//ASTのノードを入力として受け取り、関数を実行する。
+//シンボルテーブルや環境を管理する
+public class C0Interpreter {
+	
+	private List<Identifier> symbolTable; //シンボルテーブル
+	private List<Environment> interpreterEnvironment; //環境
+	
+	public static void main(String args[]) {
+		
+		//インタプリタのエントリーポイント
+		String fileName = null;
+		FileReader fileReader = null;
+		AstNode program = null;
+		
+		//mainメソッドの引数をチェック。ファイル名が無ければ、警告を出して終了
+		try {
+			fileName = args[0];
+			fileReader = new FileReader(fileName);
+		} catch (java.lang.ArrayIndexOutOfBoundsException e) {
+			System.out.println("引数にファイル名が指定されていません");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return;
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが開けません");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return;
+		}
+		
+		//ファイルを渡して、パーサを実行
+		C0Language parser = new C0Language(fileReader);
+		parser.setFileName(fileName);
+		
+		//parser.enable_tracing(); //パーサのトレース機能を開始
+		
+		//構文木の出力
+		try {			
+			program = parser.file();
+			int depth = 0;
+			program.dump(depth, true);
+		} catch (ParseException e1) {
+			// TODO 自動生成された catch ブロック
+			e1.printStackTrace();
+		}
+		
+		//インタプリタが構文木を入力として受け取り、プログラムを実行する
+	}
+}
--- src/c0/interpreter/Interpreter.java
+++ src/c0/interpreter/Interpreter.java
@@ -1,63 +1,21 @@
 package c0.interpreter;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-import c0.ast.AstNode;
-import c0.parser.C0Language;
-import c0.parser.ParseException;
-import c0.util.Identifier;
-
-//ASTのノードを入力として受け取り、関数を実行する。
-//シンボルテーブルや環境を管理する
-public class Interpreter {
+public interface Interpreter {
 	
-	private List<Identifier> symbolTable; //シンボルテーブル
-	private List<Environment> interpreterEnvironment; //環境
+	//関数呼び出し
+	void executeFunctionCall();
 	
-	public static void main(String args[]) {
-		
-		//インタプリタのエントリーポイント
-		String fileName = null;
-		FileReader fileReader = null;
-		AstNode program = null;
-		
-		//mainメソッドの引数をチェック。ファイル名が無ければ、警告を出して終了
-		try {
-			fileName = args[0];
-			fileReader = new FileReader(fileName);
-		} catch (java.lang.ArrayIndexOutOfBoundsException e) {
-			System.out.println("引数にファイル名が指定されていません");
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-			return;
-		} catch (FileNotFoundException e) {
-			System.out.println("ファイルが開けません");
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-			return;
-		}
-		
-		//ファイルを渡して、パーサを実行
-		C0Language parser = new C0Language(fileReader);
-		parser.setFileName(fileName);
-		
-		//parser.enable_tracing(); //パーサのトレース機能を開始
-		
-		//構文木の出力
-		try {			
-			program = parser.file();
-			int depth = 0;
-			program.dump(depth, true);
-		} catch (ParseException e1) {
-			// TODO 自動生成された catch ブロック
-			e1.printStackTrace();
-		}
-		
-		//インタプリタが構文木を入力として受け取り、プログラムを実行する
-	}
+	//文の処理
+	void executeStatement(); //複合文の中で使用する
+	void executeBlockStatement();
+	void executeIfStatement();
+	void executeWhileStatement();
+	void executeForStatement();
+	void executeBreakStatement();
+	void executeReturnStatement();
+	void executeExpressionStatement();
+	
+	//式文の中で使用する
+	
+	
 }
