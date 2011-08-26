141dd39a7e6c3408341c68f243def0fd35b78498
kimuraya <wired_rar@hotmail.com>
2011-08-27 00:53:33 +0900

    クラスSymbolTableのメソッドを実装した

--- src/c0/util/SymbolTable.java
+++ src/c0/util/SymbolTable.java
@@ -4,24 +4,76 @@ import java.util.LinkedList;
 
 public class SymbolTable {
 	
+	//シンボルテーブルには同じ名前の識別子
 	private LinkedList<Identifier> symbolTable = new LinkedList<Identifier>();
 	
 	//識別子がシンボルテーブルに登録済みかをチェックする
 	public boolean searchSymbol(Identifier Symbol) {
+		
+		//指定された要素が存在する場合
+		if (this.symbolTable.indexOf(Symbol) != -1) {
+			return true;
+		} else {
+			//指定された要素が存在しない
+			return false;
+		}
+	}
+	
+	//識別子がシンボルテーブルに登録済みかをチェックする
+	public boolean searchSymbol(String name) {
+		
+		//名前の検索
+		for (Identifier symbol : this.symbolTable) {
+			
+			//指定された要素が存在する場合
+			if (symbol.getName().equals(name)) {
+				return true;
+			}
+		}
+		
+		//指定された要素が存在しない
+		return false;
 	}
 	
 	//識別子をシンボルテーブルに追加する
 	public void addSymbol(Identifier Symbol) {
-		
+		this.symbolTable.add(Symbol);
 	}
 	
 	//名前でシンボルテーブルを検索し、シンボルを取得する
 	public Identifier getSymbol(String name) {
 		
+		//名前の検索
+		for (Identifier symbol : this.symbolTable) {
+			
+			//名前が一致した場合
+			if (symbol.getName().equals(name)) {
+				return symbol;
+			}
+		}
+		
+		//一致する要素が無かった場合
+		return null;
 	}
 	
 	//同名の識別子を更新する
-	public void updateSymbol(Identifier Symbol) {
+	public boolean updateSymbol(Identifier Symbol) {
 		
+		int cnt = 0;
+		
+		//名前の検索
+		for (Identifier updateSymbol : this.symbolTable) {
+			
+			//名前が一致した場合、更新を行う
+			if (updateSymbol.getName().equals(Symbol.getName())) {
+				this.symbolTable.set(cnt, Symbol);
+				return true;
+			}
+			
+			cnt++;
+		}
+		
+		//更新をしなかった場合
+		return false;
 	}
 }
